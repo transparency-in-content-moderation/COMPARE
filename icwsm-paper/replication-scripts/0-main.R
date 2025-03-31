cada2355a176dffa00035db1ff3e4d622dcc4e1c
MiaNahrgang <mia.nahrgang@uni-konstanz.de>
2025-03-31 13:02:01 +0200

    Add requirements to 0-main.R

--- icwsm-paper/replication-scripts/0-main.R
+++ icwsm-paper/replication-scripts/0-main.R
@@ -1,11 +1,19 @@
-library(readtext)
-library(tidyverse)
-library(commonmark)
-library(quanteda)
-library(quanteda.textstats)
-library(patchwork)
-library(ggrepel)
-library(here)
+# Installing and loading packages
+required <- c("readtext", "tidyverse", "commonmark", "quanteda","quanteda.textstats",
+              "patchwork", "ggrepel", "here")  
+
+# Check if packages are already installed, and install if missing
+new_packages <- required[!(required %in% installed.packages()[, "Package"])]
+if (length(new_packages)) install.packages(new_packages)
+
+library(readtext) # version 0.91
+library(tidyverse) # version 2.0.0
+library(commonmark) # version 1.9.1
+library(quanteda) # version 4.0.2
+library(quanteda.textstats) # version 0.97
+library(patchwork) # version 1.2.0
+library(ggrepel) # version 0.9.5
+library(here) # version 1.0.1
 
 # Setting working directory
 setwd(here("icwsm-paper/replication-scripts"))
